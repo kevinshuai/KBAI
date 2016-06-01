7fed3963686cec944a65ec0f1af5a3442b55f4b7
Jacob Kilver <robobookworm@gmail.com>
2016-05-31 21:46:31 -0400

    Agent answering some 2x2 matrices correctly

--- Agent.py
+++ Agent.py
@@ -62,101 +62,144 @@ class Agent:
     # transformations to from C to D and from B to D 
     def Solve2x2RPM(self):
         best_answer = -1
-        best_confidence = 0.0
+        confidence = 0.0
         best_AB_transformation = "NoTransform"
         best_AC_transformation = "NoTransform"
 
         # Find A to B transformation
-        best_AB_transformation = self.FindA2BTransform()
+        best_AB_transformation, AB_confidence = self.FindA2BTransform()
 
         # Find A to C transformation
-        best_AC_transformation = self.FindA2CTransform()
+        best_AC_transformation, AC_confidence = self.FindA2CTransform()
 
-        # Apply given transforms
-        #temp_best_answer = self.ApplyBestTransforms(best_AB_transformation, best_AC_transformation)  
-
-
-
-
-
-
-
-        # No transformation - just take A and compare to B
-
-        # Rotation
-
-        # Reflection
-
-        # By difference I mean absolute value difference
-        # Find difference between A and B and between A and C
-        # The difference between A and B subtract from C (x)
-        # The difference between A and C subtract from B (y)
-        # Combine the two differences (x and y)
-
-        # I am wondering if I need an operator to tell me what is in B that is not in A (what is new in B)
-        # Some things are just simple addition and subtraction. In some cases between A and B some things are
-        # removed, but some things are added. 
-        # Would just B - A work and not take the absolute value? This would start with B and remove everything that
-        # is in A.
+        # Apply given transforms and compare to answers, returning best one
+        best_answer, confidence = self.ApplyBestTransforms(best_AB_transformation, best_AC_transformation)  
     
-        return -1, 0.0
+        return best_answer, confidence
     #end def
 
     # Finds the best transform between A and B for a 2x2 matrix
     def FindA2BTransform(self):
-        best_transform = "NoTransformation"
-        least_diff = 1.0
-        best_confidence = 0.0
-
-        # No transform
-        # Compare A to B directly
-        diff = self.Compare2Images(self.A, self.B);
-        if diff < least_diff:
-            least_diff = diff
-            best_transform = "NoTransformation"
-        
-
-        # Rotation
-
-        # Reflection
-
-        # AND
-
-        # OR
-
-        confidence = 1.0-least_diff
+        best_transform, confidence = self.FindBestTransformation(self.A, self.B)
 
         return best_transform, confidence
     #end def
 
     # Finds the best transformation between A and C for a 2x2 matrix
     def FindA2CTransform(self):
+        best_transform, confidence = self.FindBestTransformation(self.A, self.C)
+
+        return best_transform, confidence
+    #end def
+
+    def FindBestTransformation(self, imgA, imgB):
         best_transform = "NoTransformation"
+        least_diff = 1.0
+        imgA_xform = imgA
 
         # No transform
-        
+        diff = self.Compare2Images(imgA_xform, imgB)
+        if diff < least_diff:
+            least_diff = diff
+            best_transform = "NoTransformation"
 
         # Rotation
+        # 90 CW
+        imgA_xform = imgA.rotate(90)
+        diff = self.Compare2Images(imgA_xform, imgB)
+        if diff < least_diff:
+            least_diff = diff
+            best_transform = "Rotation90CW"
 
+        # 90 CCW
+        imgA_xform = imgA.rotate(270)
+        diff = self.Compare2Images(imgA_xform, imgB)
+        if diff < least_diff:
+            least_diff = diff
+            best_transform = "Rotation90CCW"
+        
         # Reflection
+        # Vertical reflection - along vertical axis
+        imgA_xform = imgA.transpose(Image.FLIP_LEFT_RIGHT)
+        diff = self.Compare2Images(imgA_xform, imgB)
+        if diff < least_diff:
+            least_diff = diff
+            best_transform = "ReflectionVertical"
 
-        # AND
+        # Horizontal reflection - along horizontal axis  
+        imgA_xform = imgA.transpose(Image.FLIP_TOP_BOTTOM)
+        diff = self.Compare2Images(imgA_xform, imgB)
+        if diff < least_diff:
+            least_diff = diff
+            best_transform = "ReflectionHorizontal"  
 
-        # OR
+        # And
 
-        return best_transform
+        # OR    
+        confidence = 1.0 - least_diff
+        return best_transform, confidence
     #end def
 
     # This method will run through a number of image transformations and attempt to
-    # find the best solution to for a 2x2 RPM
+    # find the best solution to for a 3x3 RPM
     def Solve3x3RPM(self):
 
         return -1, 0.0
     #end def
 
+    # Takes the given transforms and apply to the input images
+    # Also compares to the answer choices
+    def ApplyBestTransforms(self, AB_transform, AC_transform):
+        answer = -1
+        confidence = 0.0
+
+        # Apply the AB transform to C
+        AB_C_img = self.ApplyTransform(self.C, AB_transform)
+        # See if that matches
+        temp_answer, temp_diff = self.CompareGuessToAnswers(AB_C_img)
+        if (1.0-temp_diff > confidence):
+            answer = temp_answer
+            confidence = 1.0-temp_diff
+
+        # Apply the AC transform to B
+        AC_B_img = self.ApplyTransform(self.B, AC_transform)
+        # See how well that matches
+        temp_answer, temp_diff = self.CompareGuessToAnswers(AC_B_img)
+        if (1.0-temp_diff > confidence):
+            answer = temp_answer
+            confidence = 1.0-temp_diff
+
+        # Apply AB then AC
+        
+
+        # Apply AC then AB
+
+
+
+        return answer, confidence
+    #end def
+
+    # Apply the given transform to the given image
+    def ApplyTransform(self, image, transform):
+        xform_img = image 
+
+        if transform == "NoTransformation":
+            xform_img = image
+        elif transform == "Rotation90CW":
+            xform_img = image.rotate(90)
+        elif transform == "Rotation90CCW":
+            xform_img = image.rotate(270)
+        elif transform == "ReflectionVertical":
+            xform_img = image.transpose(Image.FLIP_LEFT_RIGHT)
+        elif transform == "ReflectionHorizontal":
+            xform_img = image.transpose(Image.FLIP_TOP_BOTTOM)
+
+        return xform_img
+    #end def
 
     # This method takes in an image that represents a potential answer
-    # Returns a value between 0 and 1 that represent the similarity
+    # Returns a value between 0 and 1 that represent the similarity along with the index
+    # of the best match
     def CompareGuessToAnswers(self, guess):
         diff = 1.0 
         best_match = -1
@@ -164,7 +207,7 @@ class Agent:
         if(self.rpm.problemType == "2x2"):
             for i in range(1,7):
                 # Find the percent difference between guess and answer
-                self.Compare2Images(guess, self.answers[str(i)])
+                temp_diff = self.Compare2Images(guess, self.answers[str(i)])
                 
                 if temp_diff < diff:
                     best_match = i
@@ -196,8 +239,18 @@ class Agent:
         # are some artifacts
         
 
-        # TODO Find the total number of pixels that are different (black)
+        # TODO Find the total number of pixels that are different (white)
+        # http://codereview.stackexchange.com/questions/55902/fastest-way-to-count-non-zero-pixels-using-python-and-pillow
         num_pixels_diff = 0
+        bbox = img_diff.getbbox()
+        if not bbox: 
+            num_pixels_diff = 0
+        else: 
+            num_pixels_diff = sum(img_diff.crop(bbox)
+               .point(lambda x: 255 if x else 0)
+               .convert("L")
+               .point(bool)
+               .getdata())
 
         # Calculate the percentage
         diff_percentage = float(num_pixels_diff)/img_diff_size  
